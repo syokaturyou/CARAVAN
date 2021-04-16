1d832d78b6c7fc4b09d9a02f2a7a41796871f9b7
EC2 Default User <ec2-user@ip-172-31-49-230.ec2.internal>
2021-04-16 04:21:47 +0000

    [Finish]4-5

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,22 +1,22 @@
 class BlogsController < ApplicationController
 
   def show
+    @blog = Blog.find(params[:id])
   end
-
 
   def index
     @blogs = Blog.all
   end
 
-
-　def new
-    @blog = Blog.new
-  end
+  def new
+    @blog = Blog.new end
 
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path  end
+    redirect_to blog_path(blog.id)
+
+  end
 
   def edit
   end
@@ -25,4 +25,5 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
+
 end
